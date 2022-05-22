b3079b3815240bcff238599de5f2322f68d9cca5
Michael Roeslein <michaeljroeslein@gmail.com>
2022-05-22 12:00:06 -0400

    Update spotify endpoint

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -10,7 +10,7 @@ const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
-const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played`;
+const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played?limit=50`;
 
 const getAccessToken = async () => {
   const response = await fetch(TOKEN_ENDPOINT, {
