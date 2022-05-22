af015cc64e88a7ced0424237a6e75665bd8ea17e
Michael Roeslein <michaeljroeslein@gmail.com>
2022-05-22 12:07:41 -0400

    Update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,12 @@
 import Page from "components/PageLayout";
-import PageHeader from "components/PageHeader";
 
 export default function AboutPage() {
   return (
     <Page title="About">
-      <PageHeader>
-        <h1>About</h1>
-        <p>
-          Hi, my name's Michael and I'm a design-focused web developer based out
-          of New York.
-        </p>
-      </PageHeader>
+      <p>
+        Hi, my name's Michael and I'm a design-focused web developer based out
+        of New York.
+      </p>
     </Page>
   );
 }
